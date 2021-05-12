e41b49938ac8b4202a7c603fcf805a433786de61
mayhixza <emailwithmoi@gmail.com>
2021-05-12 18:33:10 +0530

    searchbar placeholder

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -160,8 +160,8 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
-          <input placeholder="Where? Enter pincode" onChange={this.handleLocationChange}/>
+          <input placeholder="Search businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where? " onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
           <a onClick={this.handleSearch}>Let's Go</a>
