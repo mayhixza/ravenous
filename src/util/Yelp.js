a867af60ed6610433eb3b91b33391ceb4c6f25d6
mayhixza <emailwithmoi@gmail.com>
2021-05-13 18:25:25 +0530

    cors anywhere

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,7 +1,7 @@
 const apiKEY = process.env.REACT_APP_API_KEY;
 const Yelp = {
     search(term, location, sortBy) {
-        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
             headers: {
                 Authorization: `Bearer ${apiKEY}`
             }
